ddd59d2a58febd962f1df14eb2f02befdcbed19e
Krunal-smiles <34149628+Krunal-smiles@users.noreply.github.com>
2020-12-16 10:50:17 +1100

    Create init.pp

---
+++ puppet/linux/install_mdatp/manifests/init.pp
@@ -0,0 +1,59 @@
+# Puppet manifest to install Microsoft Defender ATP.
+# @param channel The release channel based on your environment, insider-fast or prod.
+# @param distro The Linux distribution in lowercase. In case of RedHat, Oracle EL, and CentOS 8, the distro variable should be 'rhel'.
+# @param version The Linux distribution release number, e.g. 7.4.
+
+class install_mdatp (
+$channel = 'insiders-fast',
+$distro = undef,
+$version = undef
+){
+    case $::osfamily {
+        'Debian' : {
+            apt::source { 'microsoftpackages' :
+                location => "https://packages.microsoft.com/${distro}/${version}/prod",
+                release  => $channel,
+                repos    => 'main',
+                key      => {
+                    'id'     => 'BC528686B50D79E339D3721CEB3E94ADBE1229CF',
+                    'server' => 'keyserver.ubuntu.com',
+                },
+            }
+        }
+        'RedHat' : {
+            yumrepo { 'microsoftpackages' :
+                baseurl  => "https://packages.microsoft.com/${distro}/${version}/${channel}",
+                descr    => "packages-microsoft-com-prod-${channel}",
+                enabled  => 1,
+                gpgcheck => 1,
+                gpgkey   => 'https://packages.microsoft.com/keys/microsoft.asc'
+            }
+        }
+        default : { fail("${::osfamily} is currently not supported.") }
+    }
+
+    case $::osfamily {
+        /(Debian|RedHat)/: {
+            file { ['/etc/opt', '/etc/opt/microsoft', '/etc/opt/microsoft/mdatp']:
+                ensure => directory,
+                owner  => root,
+                group  => root,
+                mode   => '0755'
+            }
+
+            file { '/etc/opt/microsoft/mdatp/mdatp_onboard.json':
+                source  => 'puppet:///modules/mdatp/mdatp_onboard.json',
+                owner   => root,
+                group   => root,
+                mode    => '0600',
+                require => File['/etc/opt/microsoft/mdatp']
+            }
+
+            package { 'mdatp':
+                ensure  => 'installed',
+                require => File['/etc/opt/microsoft/mdatp/mdatp_onboard.json']
+            }
+        }
+        default : { fail("${::osfamily} is currently not supported.") }
+    }
+}
